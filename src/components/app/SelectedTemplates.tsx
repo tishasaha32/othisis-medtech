20cbab1273ffb2d142a65569fc182d57c54ecb0d
Tisha Saha <tishasaha32@gmail.com>
2025-04-14 22:08:30 +0530

    Refactor SelectedTemplates component to improve layout and styling for drag-and-drop functionality

--- src/components/app/SelectedTemplates.tsx
+++ src/components/app/SelectedTemplates.tsx
@@ -29,38 +29,42 @@ export function SelectedTemplates({ templates, activeId }: SelectedTemplatesProp
         <div
             id="selected-templates"
             ref={setNodeRef}
-            className={`relative bg-white overflow-y-auto h-[calc(100vh-14rem)] rounded-md p-5 ${isOver ? "after:absolute after:inset-5 after:border-2 after:border-dashed after:border-blue-400 after:rounded-lg after:pointer-events-none" : ""
-                }`}
+            className="relative bg-white overflow-y-auto h-[calc(100vh-14rem)] rounded-md"
         >
-            {templates.length === 0 ? (
-                <div className="flex flex-col h-full items-center justify-center text-gray-400">
-                    <p className="flex flex-col items-center justify-center text-lg"><FileUp size={50} />Drag templates here</p>
-                </div>
-            ) : (
-                <SortableContext items={sortedTemplates.map(t => t.id)} strategy={verticalListSortingStrategy}>
-                    <div className="space-y-3">
-                        {sortedTemplates.map((template, index) => (
-                            <div
-                                key={template.id}
-                                id={`selected-template-${template.id}`}
-                                className="relative"
-                            >
-                                {isOver && activeId && index === 0 && (
-                                    <div className="absolute inset-x-0 -top-3 h-1 bg-blue-400 rounded-full transform -translate-y-1/2 z-10" />
-                                )}
-                                <TemplateCard
-                                    template={template}
-                                    isDraggable={true}
-                                    rootCanal={true}
-                                />
-                                {isOver && activeId && (
-                                    <div className="absolute inset-x-0 -bottom-3 h-1 bg-blue-400 rounded-full transform translate-y-1/2 z-10" />
-                                )}
-                            </div>
-                        ))}
+            <div className={`h-full ${isOver ? "border-2 border-dashed border-gray-300 m-5 rounded-lg" : "p-5"}`}>
+                {templates.length === 0 ? (
+                    <div className="flex flex-col h-full items-center justify-center text-gray-400">
+                        <p className="flex flex-col items-center justify-center text-lg">
+                            <FileUp size={50} />
+                            Drag templates here
+                        </p>
                     </div>
-                </SortableContext>
-            )}
+                ) : (
+                    <SortableContext items={sortedTemplates.map(t => t.id)} strategy={verticalListSortingStrategy}>
+                        <div className="space-y-3">
+                            {sortedTemplates.map((template, index) => (
+                                <div
+                                    key={template.id}
+                                    id={`selected-template-${template.id}`}
+                                    className="relative"
+                                >
+                                    {isOver && activeId && index === 0 && (
+                                        <div className="absolute inset-x-0 -top-3 border-t-2 border-dashed border-gray-300 z-10" />
+                                    )}
+                                    <TemplateCard
+                                        template={template}
+                                        isDraggable={true}
+                                        rootCanal={true}
+                                    />
+                                    {isOver && activeId && (
+                                        <div className="absolute inset-x-0 -bottom-3 border-t-2 border-dashed border-gray-300 z-10" />
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                    </SortableContext>
+                )}
+            </div>
         </div>
     );
 }
