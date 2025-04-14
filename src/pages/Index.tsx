6f0db954273ddfe8c9d67b918523dd894ea35317
Tisha Saha <tishasaha32@gmail.com>
2025-04-14 10:14:01 +0530

    Refactor App component to implement routing with React Router, replacing the initial setup with a new Index page.

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+import TemplateManager from '@/components/app/TemplateManager'
+
+const Index = () => {
+    return (
+        <div className="min-h-screen">
+            <TemplateManager />
+        </div>)
+}
+
+export default Index
